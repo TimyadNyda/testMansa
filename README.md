3ccd148e38ab28dbbecc4aa51921888003bede15
root <root@LAPTOP-QI00UP8T.localdomain>
2021-12-23 23:28:20 +0100

    small debug

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # **testMansa**
 
-Python version : 3.7.12
-Pour info, le notebook a été rédigé dans google colab.
+
+Pour info, le notebook a été rédigé dans google colab (Python 3.7.12). Et l'api en local (Python 3.8).
+L'une ou l'autre version devrait suffire pour le notebook et l'api. Le notebook peut nécessiter l'installation 
+de ipykernel (pip install ipykernel) si celui-ci est lancé à partir de VSCode par exemple. 
 
 Le parti pris ici est d'estimer des intervalles de prédiction pour la somme des dépenses des 30 jours suivants. 
 La démarche est inspirée du papier suivant : http://proceedings.mlr.press/v80/pearce18a/pearce18a.pdf
--- main.py
+++ main.py
@@ -65,16 +65,17 @@ def predict(transactions: List[Transaction], account: Account) -> float:
     # process values and keep last 180 timesteps
     df_ = preprocess_values(df_)[-180:]
 
-    # Return predicted amount
     preds = model.predict(np.expand_dims(df_, 0))[0]
+
     u_bound = np.clip(preds[0], 0, preds[0])
     l_bound = np.clip(preds[1], 0, preds[1])
+
+    #rescale outputs
     if not np.equal(u_bound, 0):
         u_bound = np.exp(u_bound*MAX_LOG_TARGET)
     if not np.equal(l_bound, 0):
         l_bound = np.exp(l_bound*MAX_LOG_TARGET)
-
-    np.equal(0., 0)
+        
     return ResponsePredict(
         predicted_amount=(u_bound + l_bound) / 2,
         upper_bound=u_bound,
